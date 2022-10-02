7829e65332fc64e591928bfd5168bb6fb0097754
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2022-10-03 00:30:50 +0530

    index.html done

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-btn");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-function clickHandler() {
-    console.log(billAmount.value, cashGiven.value);
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 2, 1];
+
+function validateBillAndCashAmount() {
+    hideMessage();
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("The cash provided should atleast be equal to the bill amount");
+        }
+    } else {
+        showMessage("Invalid Bill Amount");
+    }
 }
 
+function hideMessage() {
+    message.style.display = "none";
+}
 
-checkButton.addEventListener('click', clickHandler);
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
+
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+checkButton.addEventListener('click', validateBillAndCashAmount);
